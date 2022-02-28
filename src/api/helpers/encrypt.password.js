c53652869d615fe24bf1ac6fedacb5a408d07d85
Andhy Ralac <andhyralac11@gmail.com>
2022-02-27 22:10:56 -0600

    commented encrypt password function

--- src/api/helpers/encrypt.password.js
+++ src/api/helpers/encrypt.password.js
@@ -1,5 +1,10 @@
 const bcryptjs = require('bcryptjs');
 
+/**
+ * Function to encrypt password
+ * @param {string} password 
+ * @returns {string} encryptedPassword
+ */
 const encryptPassword = (password) => {
     const salt = bcryptjs.genSaltSync();
     const passwordEncrypt = bcryptjs.hashSync(password, salt);
