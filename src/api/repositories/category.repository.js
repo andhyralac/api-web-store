17d95495a5c80f2e762d118ec5f864ad684f0190
Andhy Ralac <andhyralac11@gmail.com>
2022-03-07 23:37:31 -0600

    category repository creation

---
+++ src/api/repositories/category.repository.js
@@ -0,0 +1,11 @@
+const BaseRepository = require('./base.repository');
+const Category = require('../models/category.model');
+
+class CategoryRepository extends BaseRepository {
+    constructor() {
+        super(Category);
+    }
+}
+
+
+module.exports = new CategoryRepository;
